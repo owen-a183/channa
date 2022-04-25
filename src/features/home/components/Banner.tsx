555879c715852f26bca4004e2f44ff331edc1b32
oniharnantyo <oni.harnantyo97@gmail.com>
2022-04-25 22:23:29 +0700

    feat: added banner

---
+++ src/features/home/components/Banner.tsx
@@ -0,0 +1,34 @@
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import HomeBanner from '@/assets/home-banner.jpg';
+
+export const Banner = () => {
+  return (
+    <section
+      className="banner-area d-flex text-center"
+      style={{
+        backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${HomeBanner})`,
+      }}
+    >
+      <Container className="align-self-center">
+        <Row>
+          <Col md={12}>
+            <div className="banner-content">
+              <h5 className="banner-content-h5">VIHĀRA VIDYĀLOKA</h5>
+              <h1 className="banner-content-h1">VIDYĀSENĀ</h1>
+              <p className="banner-content-description">
+                Organisasi sosial pemuda Buddha yang bermarkas di Vihara Vidyaloka Yogyakarta.
+                Didirikan pada tanggal 1 Februari 1987, telah menjadi salah satu organisasi pemuda
+                Buddhis utama di bawah bimbingan Sangha Theravada Indonesia (STI).
+              </p>
+              <LinkContainer to="/tentang-kami">
+                <Button className="btn-banner">Baca Selengkapnya</Button>
+              </LinkContainer>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
---
+++ src/features/home/routes/Home.tsx
@@ -0,0 +1,10 @@
+import { Banner } from '../components/Banner';
+
+export const Home = () => {
+  return (
+    <>
+      <Banner />
+      <h1>Home Page</h1>
+    </>
+  );
+};
